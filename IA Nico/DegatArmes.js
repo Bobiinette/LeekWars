f087197f50f20d0308de20d5cca86de5e26c5a95
Bobiinette <nic1212c@gmail.com>
2016-12-06 23:08:20 +0100

    Corrections mult shield effacé Il faut faire les calculs de TP avec les chip (avec prise en compte des cooldown)

--- IA Nico/DegatArmes.js
+++ IA Nico/DegatArmes.js
@@ -6,7 +6,7 @@ function diffPositive(a, b) {
 	return 0;
 }
 
-function getDamageWeapon(weapon, attackLeekStrenght, attackLeekTP, shield, defensiveLeekResitance, @degatsMin, @degatsMax) {
+function getDamageWeapon(weapon, attackLeekStrenght, attackLeekTP, shield, @degatsMin, @degatsMax) {
 	/*weapon : l'arme de l'adversaire
 	attackLeekStrenght : attack du leek attaquant
 	attackLeekTP : le nombre de TP MAX du leek attaquant
@@ -18,87 +18,85 @@ function getDamageWeapon(weapon, attackLeekStrenght, attackLeekTP, shield, defen
 	//Ni du gazeur
 	var multDamages=attackLeekStrenght;
 	multDamages=multDamages/100;
-	var multShield=defensiveLeekResitance;
-	multShield=multShield/100;
 	var pond=0;
 	if(weapon==WEAPON_PISTOL) {
 		pond=attackLeekTP/3;
-		degatsMin=max(degatsMin,diffPositive(15*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(20*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(15*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(20*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_MACHINE_GUN) {
 		pond=attackLeekTP/4;
-		degatsMin=max(degatsMin,diffPositive(20*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(24*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(20*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(24*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_DOUBLE_GUN) {
 		pond=attackLeekTP/4;
-		degatsMin=max(degatsMin,diffPositive(15*multDamages,shield*multShield)*pond + diffPositive(5*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(20*multDamages,shield*multShield)*pond + diffPositive(8*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(15*multDamages,shield)*pond + diffPositive(5*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(20*multDamages,shield)*pond + diffPositive(8*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_SHOTGUN) {
 		pond=attackLeekTP/5;
-		degatsMin=max(degatsMin,diffPositive(33*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(43*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(33*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(43*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_MAGNUM) {
 		pond=attackLeekTP/5;
-		degatsMin=max(degatsMin,diffPositive(25*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(40*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(25*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(40*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_BROADSWORD) {
 		pond=attackLeekTP/5;
-		degatsMin=max(degatsMin,diffPositive(39*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(41*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(39*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(41*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_LASER) {
 		pond=attackLeekTP/6;
-		degatsMin=max(degatsMin,diffPositive(43*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(59*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(43*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(59*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_AXE) {
 		pond=attackLeekTP/6;
-		degatsMin=max(degatsMin,diffPositive(44*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(77*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(44*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(77*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_GRENADE_LAUNCHER) {
 		pond=attackLeekTP/6;
-		degatsMin=max(degatsMin,diffPositive(45*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(53*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(45*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(53*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_FLAME_THROWER) {
 		pond=attackLeekTP/6;
-		degatsMin=max(degatsMin,diffPositive(35*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(40*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(35*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(40*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_DESTROYER) {
 		pond=attackLeekTP/6;
-		degatsMin=max(degatsMin,diffPositive(40*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(60*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(40*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(60*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_B_LASER) {
 		pond=attackLeekTP/5;
-		degatsMin=max(degatsMin,diffPositive(50*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(60*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(50*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(60*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_KATANA) {
 		pond=attackLeekTP/7;
-		degatsMin=max(degatsMin,diffPositive(77*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(77*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(77*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(77*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_ELECTRISOR) {
 		pond=attackLeekTP/7;
-		degatsMin=max(degatsMin,diffPositive(70*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(80*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(70*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(80*multDamages,shield)*pond);
 	}
 	if(weapon==WEAPON_M_LASER) {
 		pond=attackLeekTP/8;
-		degatsMin=max(degatsMin,diffPositive(90*multDamages,shield*multShield)*pond);
-		degatsMax=max(degatsMax,diffPositive(100*multDamages,shield*multShield)*pond);
+		degatsMin=max(degatsMin,diffPositive(90*multDamages,shield)*pond);
+		degatsMax=max(degatsMax,diffPositive(100*multDamages,shield)*pond);
 	}
 }
 
-function getDamageChip(chip, attackLeekStrenght, shield, defensiveLeekResistance, @degatsMin, @degatsMax) {
+function getDamageChip(chip, attackLeekStrenght, shield, @degatsMin, @degatsMax) {
 	/*chip : la puce de l'adversaire
 	attackLeekStrenght : attack du leek attaquant
 	shield : valeur du shield du défenseur
@@ -108,62 +106,60 @@ function getDamageChip(chip, attackLeekStrenght, shield, defensiveLeekResistance
 	//revoie 0 si la puce n'est pas une puce qui fait des dégats (ou empoisonne)
 	var multDamages=attackLeekStrenght;
 	multDamages=multDamages/100;
-	var multShield=defensiveLeekResistance;
-	multShield=multShield/100;
 	if(chip==CHIP_SHOCK) {
-		degatsMin=degatsMin+diffPositive(5*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(7*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(5*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(7*multDamages,shield);
 	}
 	else if(chip==CHIP_PEBBLE) {
-		degatsMin=degatsMin+diffPositive(2*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(17*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(2*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(17*multDamages,shield);
 	}
 	else if(chip==CHIP_SPARK) {
-		degatsMin=degatsMin+diffPositive(8*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(16*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(8*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(16*multDamages,shield);
 	}
 	else if(chip==CHIP_ICE) {
-		degatsMin=degatsMin+diffPositive(17*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(19*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(17*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(19*multDamages,shield);
 	}
 	else if(chip==CHIP_ROCK) {
-		degatsMin=degatsMin+diffPositive(30*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(31*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(30*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(31*multDamages,shield);
 	}
 	else if(chip==CHIP_FLASH) {
-		degatsMin=degatsMin+diffPositive(19*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(24*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(19*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(24*multDamages,shield);
 	}
 	else if(chip==CHIP_FLAME) {
-		degatsMin=degatsMin+diffPositive(25*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(27*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(25*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(27*multDamages,shield);
 	}
 	else if(chip==CHIP_STALACTITE) {
-		degatsMin=degatsMin+diffPositive(64*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(67*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(64*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(67*multDamages,shield);
 	}
 	else if(chip==CHIP_LIGHTNING) {
-		degatsMin=degatsMin+diffPositive(35*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(47*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(35*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(47*multDamages,shield);
 	}
 	else if(chip==CHIP_ROCKFALL) {
-		degatsMin=degatsMin+diffPositive(48*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(56*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(48*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(56*multDamages,shield);
 	}
 	else if(chip==CHIP_ICEBERG) {
-		degatsMin=degatsMin+diffPositive(72*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(80*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(72*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(80*multDamages,shield);
 	}
 	else if(chip==CHIP_METEORITE) {
-		degatsMin=degatsMin+diffPositive(70*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(80*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(70*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(80*multDamages,shield);
 	}
 	else if(chip==CHIP_DEVIL_STRIKE) {
-		degatsMin=degatsMin+5*diffPositive(25*multDamages,shield*multShield);
-		degatsMax=degatsMax+5*diffPositive(25*multDamages,shield*multShield);
+		degatsMin=degatsMin+5*diffPositive(25*multDamages,shield);
+		degatsMax=degatsMax+5*diffPositive(25*multDamages,shield);
 	}
 	else if(chip==CHIP_BURNING) {
-		degatsMin=degatsMin+diffPositive(78*multDamages,shield*multShield);
-		degatsMax=degatsMax+diffPositive(87*multDamages,shield*multShield);
+		degatsMin=degatsMin+diffPositive(78*multDamages,shield);
+		degatsMax=degatsMax+diffPositive(87*multDamages,shield);
 	}
 }
